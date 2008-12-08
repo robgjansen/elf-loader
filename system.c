ce2cfbae26ec765b53297c486ae25166ae02ae9e
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2008-12-08 17:23:47 +0100

    wrapper around syscalls

---
+++ system.c
@@ -0,0 +1,20 @@
+#include "system.h"
+#include "syscall.h"
+#include <sys/mman.h>
+
+uint8_t *system_mmap_anon (size_t size)
+{
+  unsigned long start = SYSCALL6(mmap2, 0, size, PROT_READ | PROT_WRITE, 
+				 MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+  return (uint8_t *)start;
+}
+uint8_t *system_mmap_anon_with_position (unsigned long position, size_t size)
+{
+  unsigned long start = SYSCALL6(mmap2, position, size, PROT_READ | PROT_WRITE, 
+				 MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+  return (uint8_t *)start;
+}
+void system_munmap (uint8_t *start, size_t size)
+{
+  SYSCALL2 (munmap, start, size);
+}
---
+++ system.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#include <stdint.h>
+#include <sys/types.h>
+
+uint8_t *system_mmap_anon (size_t size);
+uint8_t *system_mmap_anon_with_position (unsigned long position, size_t size);
+void system_munmap (uint8_t *start, size_t size);
+
+#endif /* SYSTEM_H */
