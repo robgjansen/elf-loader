cd248c6598fe13397c3e28778c2548adc5348284
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2009-02-05 10:31:21 +0100

    add print output

--- test/hello-dlopen.c
+++ test/hello-dlopen.c
@@ -6,6 +6,7 @@ int main (int argc, char *argv[])
 {
   printf ("enter main\n");
   void *h = dlopen ("libtest.so", RTLD_LAZY);
+  printf ("dlopen completed\n");
   dlclose (h);
   printf ("leave main\n");
   return 0;
