7cd28c3099175f5cc61cca8c2dfb1af468e5d22b
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2009-02-05 14:36:34 +0100

    another test

--- test/libf.c
+++ test/libf.c
@@ -1,3 +1,15 @@
 #include "test.h"
 
 LIB(f)
+
+extern void function_e (void);
+
+void function_e (void)
+{
+  printf ("called function_e in libf.so\n");
+}
+
+void function_f_e (void)
+{
+  function_e ();
+}
