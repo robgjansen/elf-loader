415f950d4bd47424f52df475bdfd52d06439fb50
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2009-07-21 16:40:58 +0200

    add error codes

--- test/test9.c
+++ test/test9.c
@@ -16,7 +16,7 @@ static void *thread_a (void *ctx)
 	{
 	  return (void*)-1;
 	}
-      printf ("count=%d\n", g_count);
+      printf ("a=%d\n", g_count);
       status = sem_post (&g_sem_b);
       if (status == -1)
 	{
@@ -36,7 +36,7 @@ static void *thread_b (void *ctx)
 	{
 	  return (void*)-1;
 	}
-      printf ("count=%d\n", g_count);
+      printf ("b=%d\n", g_count);
       status = sem_post (&g_sem_a);
       if (status == -1)
 	{
@@ -53,41 +53,41 @@ int main (int argc, char *argv[])
   int status = sem_init (&g_sem_a, 0, 1);
   if (status == -1)
     {
-      return -1;
+      return 1;
     }
   status = sem_init (&g_sem_b, 0, 0);
   if (status == -1)
     {
-      return -1;
+      return 2;
     }
   pthread_attr_t attr;
   status = pthread_attr_init(&attr);
   if (status == -1)
     {
-      return -1;
+      return 3;
     }
   pthread_t tha;
   status = pthread_create (&tha, &attr, &thread_a, 0);
   if (status == -1)
     {
-      return -1;
+      return 4;
     }
   pthread_t thb;
   status = pthread_create (&thb, &attr, &thread_b, 0);
   if (status == -1)
     {
-      return -1;
+      return 5;
     }
   void *retval;
   status = pthread_join (tha, &retval);
   if (status == -1 || retval != 0)
     {
-      return -1;
+      return 6;
     }
   status = pthread_join (thb, &retval);
   if (status == -1 || retval != 0)
     {
-      return -1;
+      return 7;
     }
   return 0;
 }
