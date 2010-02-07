ae1568a77f75c89e834360d8094afdb476cdf087
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2010-02-07 18:45:12 +0100

    add comment

--- vdl-sort.h
+++ vdl-sort.h
@@ -4,6 +4,7 @@
 struct VdlFileList;
 struct VdlFile;
 
+// must free return value with vdl_file_list_free
 struct VdlFileList *vdl_sort_increasing_depth (struct VdlFileList *files);
 struct VdlFileList *vdl_sort_deps_breadth_first (struct VdlFile *file);
 struct VdlFileList *vdl_sort_call_init (struct VdlFileList *files);
