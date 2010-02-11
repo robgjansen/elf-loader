d4bb7a42c8d21ec2e7be5e44c69d2f6688761baf
Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
2010-02-11 22:33:47 +0100

    failing tests

--- internal-test-array.cc
+++ internal-test-array.cc
@@ -14,6 +14,15 @@ bool test_array (void)
   INTERNAL_TEST_ASSERT_EQ (vdl_array_get_size (a), 2);
   INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 0, int), 5);
   INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 1, int), 10);
+  vdl_array_prepend (a, 1);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get_size (a), 3);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 0, int), 1);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 1, int), 5);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 2, int), 10);
+  vdl_array_remove (a, 0);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get_size (a), 2);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 0, int), 5);
+  INTERNAL_TEST_ASSERT_EQ (vdl_array_get (a, 2, int), 10);
   vdl_array_delete (a);
   return true;
 }
